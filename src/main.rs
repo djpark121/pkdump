8b3a5ca8ebffa02b116ff0370ad5510211c72e1d
David Park <djpark121@proton.me>
2025-01-06 20:33:51 -0600

    Added basic arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "pkdump is a packet sniffer written in Rust", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    count: Option<usize>,
+
+    #[arg(short, long)]
+    interface: Option<String>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    if let Some(i) = args.interface {
+        println!("Capturing on {}", i);
+    }
+
+    if let Some(count) = args.count {
+        println!("{} packets requested", count);
+    }
+
+    Ok(())
 }
