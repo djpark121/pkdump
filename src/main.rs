e9ef6c09d625eea79e887e8d773801b58295ea60
Egil Rasmussen <erasmussen1@gmail.com>
2025-01-07 10:07:30 -0800

    Move args variables into it own file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,11 @@
 use clap::Parser;
 use pcap::Device;
 
-#[derive(Parser, Debug)]
-#[command(version, about = "pkdump is a packet sniffer written in Rust", long_about = None)]
-struct PkdumpArgs {
-    /// Count
-    #[arg(short, long)]
-    count: Option<usize>,
+mod args;
 
-    /// Interface name
-    #[arg(short, long)]
-    interface: Option<String>,
-
-    /// List all devices
-    #[arg(short, long)]
-    list: bool,
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = PkdumpArgs::parse();
+    let args = args::PkdumpArgs::parse();
 
     if let Some(i) = args.interface {
         println!("Capturing on {}", i);
