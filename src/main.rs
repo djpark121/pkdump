35d58c571bfae3e6fbd9582e726dd736b6ceda4c
djpark121 <djpark121@proton.me>
2025-01-07 21:28:49 -0600

    Compiles, but doesn't seem to capture

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
+use anyhow;
 use clap::Parser;
-use pcap::Device;
+use pcap::{Capture, Device};
 
 mod args;
 
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> anyhow::Result<()> {
     let args = args::PkdumpArgs::parse();
-
-    if let Some(i) = args.interface {
-        println!("Capturing on {}", i);
-    }
-
-    if let Some(count) = args.count {
-        println!("{} packets requested", count);
-    }
 
     if args.list {
         let devices = Device::list()?;
@@ -23,7 +15,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("  {}", device.name);
         }
 
-        return Ok(())
+        return Ok(());
+    }
+
+    if let Some(i) = args.interface {
+        println!("Capturing on {}", i);
+    }
+
+    if let Some(count) = args.count {
+        println!("{} packets requested", count);
+    }
+
+    let mut cap = Capture::from_device(Device::lookup().unwrap().unwrap()) // open the "default" interface
+        .unwrap() // assume the device exists and we are authorized to open it
+        .open() // activate the handle
+        .unwrap(); // assume activation worked
+
+    while let Ok(packet) = cap.next_packet() {
+        println!("received packet! {:?}", packet);
     }
 
     Ok(())
